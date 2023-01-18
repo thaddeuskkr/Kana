a5647eba55397207ffadbe250e38d97a88e938cc
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-01-18 23:45:37 +0800

    feat(lyrics): add lyrics command
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/commands/music/lyrics.js
+++ src/commands/music/lyrics.js
@@ -17,7 +17,8 @@
 */
 
 import { Command } from '@sapphire/framework';
-// import { PaginatedMessage } from '@sapphire/discord.js-utilities';
+import Genius from 'genius-lyrics';
+import { PaginatedMessage } from '@sapphire/discord.js-utilities';
 
 export class LyricsCommand extends Command {
     constructor(context, options) {
@@ -45,53 +46,56 @@ export class LyricsCommand extends Command {
     }
     
     async chatInputRun(interaction) {
-        return interaction.reply({ embeds: [this.container.util.embed('error', 'This command is currently disabled.')] });
-        /*
-        await interaction.deferReply();
+        await interaction.reply({ embeds: [this.container.util.embed('loading', 'Fetching lyrics...')] });
         const dispatcher = this.container.queue.get(interaction.guildId);
         let query = interaction.options.getString('query');
-        if (!query && !dispatcher?.current) return interaction.editReply({ embeds: [this.container.util.embed('error', 'You did not provide a query and there is nothing playing.')], ephemeral: true });
-        query = query || `${dispatcher.current.info.title.replace('(Lyrics)', '')} - ${dispatcher.current.info.author.replace(' - Topic', '')}`; // most common things to replace
-        const lyrics = await findLyrics.LyricsFinder(query);
-        if (!lyrics || lyrics instanceof Error) {
-            return interaction.editReply({ embeds: [this.container.util.embed('error', `No results for \`${query}\`.${!interaction.options.getString('query') ? ' Try searching using a query instead.' : ''}`)] });
-        } else {
-            const lyr = LyricsCommand.splitLyrics(lyrics);
-            const pm = new PaginatedMessage();
-            for (const page of lyr) {
-                pm.addPageEmbed((embed) => {
-                    embed
-                        .setAuthor({ name: `Lyrics${!interaction.options.getString('query') ? '' : ' (Custom query)'}` })
-                        .setTitle(query)
-                        .setDescription(page)
-                        .setFooter(this.container.config.footer)
-                        .setColor('#cba6f7');
-                    return embed;
-                });
-            }
-            pm.run(interaction);
+        if (!query && !dispatcher?.current) return interaction.reply({ embeds: [this.container.util.embed('error', 'You did not provide a query and there is nothing playing.')] });
+        if (!query) query = `${dispatcher.current.info.title.replace('(Lyrics)', '')} - ${dispatcher.current.info.author.replace(' - Topic', '')}`; // most common things to replace
+        const Client = new Genius.Client();
+        const searches = await Client.songs.search(query);
+        if (!searches.length) return interaction.editReply({ embeds: [this.container.util.embed('error', `No results for \`${query}\`.${!interaction.options.getString('query') ? ' Try searching using a query instead.' : ''}`)] });
+        const rawLyrics = await searches[0].lyrics();
+        const lyrics = rawLyrics.replace(/([^\n\n])\[/g, '$1\n\n['); // Maybe format the stuff in square brackets.
+        const lyr = LyricsCommand.splitLyrics(lyrics);
+        const pm = new PaginatedMessage();
+        for (const page of lyr) {
+            pm.addPageEmbed((embed) => {
+                embed.setAuthor({ name: `Lyrics${!interaction.options.getString('query') ? '' : ' (Custom query)'}` })
+                    .setTitle(query)
+                    .setDescription(page)
+                    .setFooter(this.container.config.footer)
+                    .setColor('#cba6f7');
+                return embed;
+            });
         }
-        */
+        pm.run(interaction);
     }
 
-    async whatsappRun({ msg /*, args, dispatcher */}) {
-        return msg.reply('This command is currently disabled.');
-        /*
+    async whatsappRun({ msg, args, dispatcher}) {
         let query = args.join(' ');
         if (!query && !dispatcher?.current) return await msg.reply('You did not provide a query and there is nothing playing.');
         query = query || `${dispatcher.current.info.title.replace('(Lyrics)', '')} - ${dispatcher.current.info.author.replace(' - Topic', '')}`; // most common things to replace
-        const lyrics = await findLyrics.LyricsFinder(query);
-        if (!lyrics || lyrics instanceof Error) {
-            return msg.reply(`No results for _${query}_.${!args.length ? ' Try searching using a query instead.' : ''}`);
-        } else {
-            msg.reply(`*Lyrics${!args.length ? '' : ' (Custom query)'}*\n_${lyrics}_`);
+        const Client = new Genius.Client();
+        const searches = await Client.songs.search(query);
+        if (!searches.length) return msg.reply(`No results for _${query}_.${!args.length ? ' Try searching using a query instead.' : ''}`);
+        const rawLyrics = await searches[0].lyrics();
+        const lyrics = rawLyrics.replace(/([^\n\n])\[/g, '$1\n\n['); // Maybe format the stuff in square brackets.
+        let para = lyrics.split(/\[/g);
+        for (let i = 0; i < para.length; i++) {
+            para[i] = para[i].trim();
         }
-        */
+        const ly = para.join('\n\n[').trim();
+        msg.reply(`*Lyrics${!args.length ? ` (${dispatcher.current.info.title.replace('(Lyrics)', '')} - ${dispatcher.current.info.author.replace(' - Topic', '')})`: ' (Custom query)'}*\n${ly}`);
     }
 
     static splitLyrics (lyrics) {
+        let para = lyrics.split(/\[/g);
+        for (let i = 0; i < para.length; i++) {
+            para[i] = para[i].trim();
+        }
+        const ly = para.join('\n\n[').trim();
         const maxCharsInAPage = 2000;
-        const lineArray = lyrics.split('\n');
+        const lineArray = ly.split('\n');
         const pages = [];
         for (let i = 0; i < lineArray.length; i++) {
             let page = '';
