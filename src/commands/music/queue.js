d45a4d6a960f83e70c6457aca8702bc33ba3dcfc
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-02-06 09:37:12 +0800

    fix(queue): add voice channel checks for whatsapp
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -64,8 +64,9 @@ export class QueueCommand extends Command {
         pm.run(interaction);
     }
 
-    async whatsappRun({ msg, dispatcher, discordUser }) {
+    async whatsappRun({ msg, dispatcher, discordUser, voice }) {
         if (!discordUser) return await msg.reply('You are not linked to a Discord account. Use ```/link``` to link your WhatsApp account to your Discord account.');
+        if (voice === false) return await msg.reply('You are not in a voice channel.');
         if (!dispatcher) return await msg.reply('There is nothing playing.');
         const queue = dispatcher.queue;
         const current = dispatcher.current;
