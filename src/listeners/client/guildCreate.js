c84b7b00f4d6c3d6f0b8921d58c49454e62bee9e
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-02-12 14:26:03 +0800

    feat(listeners): add guildCreate and guildDelete listeners
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

---
+++ src/listeners/client/guildCreate.js
@@ -0,0 +1,14 @@
+import { Listener } from '@sapphire/framework';
+
+export class GuildCreateListener extends Listener {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            event: 'guildCreate'
+        });
+    }
+    async run (guild) {
+        this.container.logger.info(`New guild: ${guild.name} (${guild.id})`);
+        this.container.webhook.send({ embeds: [this.container.util.embed(`Added to server: **${guild.name}** \`${guild.id}\``)] });
+    }
+}
---
+++ src/listeners/client/guildDelete.js
@@ -0,0 +1,14 @@
+import { Listener } from '@sapphire/framework';
+
+export class GuildDeleteListener extends Listener {
+    constructor(context, options) {
+        super(context, {
+            ...options,
+            event: 'guildDelete'
+        });
+    }
+    async run (guild) {
+        this.container.logger.info(`Guild removed: ${guild.name} (${guild.id})`);
+        this.container.webhook.send({ embeds: [this.container.util.embed(`Removed from server: **${guild.name}** \`${guild.id}\``)] });
+    }
+}
