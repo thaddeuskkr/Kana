88ca730b9db7807a72a272005437fa33daa53f56
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-02-14 09:56:00 +0800

    fix(activity): maintenance fix
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -15,8 +15,8 @@ export class ReadyListener extends Listener {
         const maintenance = await client.db.get('maintenance');
         if (maintenance === undefined) await client.db.set('maintenance', false);
         setInterval(async () => {
-            const maintenance = await client.db.get('maintenance');
-            if (maintenance == true) return client.user.setPresence({ activities: [{ name: 'maintenance mode', type: 'PLAYING' }], status: 'dnd' });
+            // const maintenance = await client.db.get('maintenance');
+            // if (maintenance == true) return client.user.setPresence({ activities: [{ name: 'maintenance mode', type: 'PLAYING' }], status: 'dnd' });
             const activity = this.container.config.activities[current];
             client.user.setPresence({ activities: [activity], status: activity.status });
             current = current >= this.container.config.activities.length - 1 ? 0 : current + 1;
