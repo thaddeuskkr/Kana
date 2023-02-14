570dc3203615c3fcfef0dfa4730d58ee83ac6cfb
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-02-14 09:44:48 +0800

    feat(maintenance-mode): show maintenance mode on activity
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -12,7 +12,11 @@ export class ReadyListener extends Listener {
         const { username, id } = client.user;
         this.container.logger.info(`Logged in as ${username} (${id})`);
         let current = 0;
-        setInterval(async() => {
+        const maintenance = await client.db.get('maintenance');
+        if (maintenance === undefined) await client.db.set('maintenance', false);
+        setInterval(async () => {
+            const maintenance = await client.db.get('maintenance');
+            if (maintenance == true) return client.user.setPresence({ activities: [{ name: 'maintenance mode', type: 'PLAYING' }], status: 'dnd' });
             const activity = this.container.config.activities[current];
             client.user.setPresence({ activities: [activity], status: activity.status });
             current = current >= this.container.config.activities.length - 1 ? 0 : current + 1;
