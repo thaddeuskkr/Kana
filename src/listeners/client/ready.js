00ece11af64b5ebc175e6e25bda7227d2f489eb4
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-02-17 11:33:35 +0800

    feat(ready): better logging for event
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/listeners/client/ready.js
+++ src/listeners/client/ready.js
@@ -16,14 +16,17 @@ export class ReadyListener extends Listener {
             this.container.motd = await this.container.db.get('motd') || { enabled: false };
             const maintenance = await this.container.db.get('maintenance') || false;
             if (maintenance == true && this.container.statusRotator) { // Set presence to maintenance if maintenance is enabled
+                this.container.logger.info('Maintenance mode enabled.');
                 this.container.statusRotator = null;
                 clearInterval(this.container.statusRotator);
                 await client.user.setPresence({ activities: [{ name: 'maintenance mode', type: 'PLAYING' }], status: 'dnd' });
             } else if (this.container.motd.enabled == true && this.container.statusRotator) { // Set presence to MOTD presence if MOTD is enabled
+                this.container.logger.info('MOTD enabled.');
                 this.container.statusRotator = null;
                 clearInterval(this.container.statusRotator);
                 await client.user.setPresence({ activities: [this.container.motd.presence], status: this.container.motd.presence.status });
             } else if (this.container.motd.enabled == false && maintenance == false && this.container.statusRotator == null) { // Set presence to status rotator if MOTD is disabled
+                this.container.logger.info('Initialising presence rotator.');
                 this.container.statusRotator = setInterval(async () => {
                     const activity = this.container.config.activities[this.container.statusRotatorCurrent];
                     client.user.setPresence({ activities: [activity], status: activity.status });
