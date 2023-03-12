c188d26104681f332576c5b8f4e1fcdbbfed54f8
Thaddeus Kuah <thaddeuskkr@gmail.com>
2023-03-12 23:12:42 +0800

    feat(info): add support server URL
    Signed-off-by: Thaddeus Kuah <thaddeuskkr@gmail.com>

--- src/commands/info/info.js
+++ src/commands/info/info.js
@@ -37,7 +37,7 @@ export class InfoCommand extends Command {
             .setTitle('About Kana')
             .setDescription(
                 'Kana is a free music Discord bot with WhatsApp integrations, made using [node.js](https://nodejs.org/) and [discord.js](https://discord.js.org/), powered by [Lavalink](https://github.com/freyacodes/Lavalink) and the [Sapphire Framework](https://sapphirejs.dev). Kana aims to provide music at the highest quality possible, fully free of charge, through a simple to use interface and command list.\n' +
-                `**[Invite](https://kana.tkkr.one/invite)** | **[GitHub](https://kana.tkkr.one/github)** | **[Vote](https://kana.tkkr.one/vote)** on **[top.gg](https://kana.tkkr.one/dbl)** | Currently on **v${this.container.client.version}**`
+                `**[Invite](https://kana.tkkr.one/invite)** | **[GitHub](https://kana.tkkr.one/github)** | **[Vote](https://kana.tkkr.one/vote)** on **[top.gg](https://kana.tkkr.one/dbl)** | **[Support](https://kana.tkkr.one/discord)** | Currently on **v${this.container.client.version}**`
             )
             .setColor('#cba6f7')
             .addFields([
